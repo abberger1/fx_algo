9a0cab6a467382990947989596006af3ff81e363
abberger1 <abberger1@gmail.com>
2016-09-21 22:58:43 -0500

    update url

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(name='oanda_fx_api',
       version=0.1,
-      description='a wrapper for the Oanda FX trading API',
-      url='https://github.com/abberger1/fx_stoch_event_algo',
+      description='a wrapper for the Oanda fxTrade REST v20 API',
+      url='https://github.com/abberger1/fx_algo',
       author='Andrew Berger',
       packages=['oanda_fx_api'],
       install_requires=[x for x in open('requirements.txt', 'r')],
